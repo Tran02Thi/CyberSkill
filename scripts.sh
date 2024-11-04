736b9e9bcba1f3ece5700c8071a8c29271d55be9
ThiTV <ThiTV@vetc.com.vn>
2024-11-04 22:28:51 +0700

    add text.txt file

--- scripts.sh
+++ scripts.sh
@@ -13,20 +13,20 @@ else
 fi
 
 
-if ls "$FOLDER_NAME"/*.pdf 1> /dev/null 2>&1; then
-	for pdf_file in "$FOLDER_NAME"/*.pdf;
-	     do
-		folder="${pdf_file%.pdf}"
+# if ls "$FOLDER_NAME"/*.pdf 1> /dev/null 2>&1; then
+# 	for pdf_file in "$FOLDER_NAME"/*.pdf;
+# 	     do
+# 		folder="${pdf_file%.pdf}"
 
-		mkdir -p "$folder"
-		pdftotext "$pdf_file" temp.txt
+# 		mkdir -p "$folder"
+# 		pdftotext "$pdf_file" temp.txt
 
-		tr -c '[:alpha:]' '[\n*]' < temp.txt | grep '^[a-zA-Z]\+$' | sort -u | while read -r word;
-			do
-				echo "$word" > "$folder/$word.txt"
-			done
-		rm temp.txt
-             done
-else
-	echo "Không có file PDF nào trong thư mục '$FOLDER_NAME'."
-fi
+# 		tr -c '[:alpha:]' '[\n*]' < temp.txt | grep '^[a-zA-Z]\+$' | sort -u | while read -r word;
+# 			do
+# 				echo "$word" > "$folder/$word.txt"
+# 			done
+# 		rm temp.txt
+#              done
+# else
+# 	echo "Không có file PDF nào trong thư mục '$FOLDER_NAME'."
+# fi
