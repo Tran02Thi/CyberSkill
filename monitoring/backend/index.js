3bc47b2d3d500a88bab53683adad3e41520c8841
ThiTV <ThiTV@vetc.com.vn>
2024-12-11 23:10:33 +0700

    add monitoring

---
+++ monitoring/backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+
+app.get('/', (req, res) => {
+	res.send('Hello World Cyberskill')
+})
+
+app.get('/user', (req, res) => {
+	res.send('Tran Thi')
+})
+
+app.listen(port, () => {
+ 	console.log("Listening on port 4000 ...")//
+})
